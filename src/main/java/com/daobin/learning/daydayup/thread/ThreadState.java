1f05f8353cfd47cf8dbf28ac74b7302f6b45ffdb
daobin <253523241@qq.com>
2021-08-02 00:55:42 +0800

    feat: init project

---
+++ src/main/java/com/daobin/learning/daydayup/thread/ThreadState.java
@@ -0,0 +1,18 @@
+package com.daobin.learning.daydayup.thread;
+
+/**
+ * @author <a href="http://git.winbaoxian.com:8888/daobin">daobin</a>
+ * @date 2021/8/1
+ */
+public class ThreadState {
+
+    public static class MyTask implements Runnable {
+
+        public void run() {
+
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
